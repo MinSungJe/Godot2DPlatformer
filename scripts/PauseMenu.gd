76c3ba3194426c0ed91d6d381ba95a2cd8c30cd6
MinSungJe <minje813@naver.com>
2023-10-19 02:34:49 +0900

    UI Animation 추가

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 onready var continueButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ContinueButton
 onready var optionsButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/OptionsButton
 onready var quitButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
+onready var panelContainerPivot = $MarginContainer/PanelContainer
+var X_
+var Y_
 
 func _ready():
 	continueButton.connect("pressed", self, "on_continue_pressed")
@@ -10,7 +13,12 @@ func _ready():
 	quitButton.connect("pressed", self, "on_quit_pressed")
 	
 	get_tree().paused = true
-	
+
+func _process(delta):
+	X_ = panelContainerPivot.rect_size.x / 2
+	Y_ = panelContainerPivot.rect_size.y / 2
+	panelContainerPivot.rect_pivot_offset = Vector2(X_,Y_)
+
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		unpause()
