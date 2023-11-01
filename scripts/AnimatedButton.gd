700ca28b2ad66c0c4cbb63a417c0611631294d0f
MinSungJe <minje813@naver.com>
2023-11-01 18:11:17 +0900

    UI와 Sound 관련 약간 수정

--- scripts/AnimatedButton.gd
+++ scripts/AnimatedButton.gd
@@ -5,18 +5,24 @@ export(bool) var disableHoverAnim
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
+	connect("hide", self, "on_button_hidden")
 	connect("pressed", self, "on_pressed")
-	connect("hide", self, "on_mouse_exited")
 
 func _process(delta):
 	rect_pivot_offset = rect_min_size / 2
+
+func reset_button_state():
+	if not disableHoverAnim: $HoverAnimationPlayer.play_backwards("hover")
 
 func on_mouse_entered():
 	if not disableHoverAnim: $HoverAnimationPlayer.play("hover")
 		
 func on_mouse_exited():
-	if not disableHoverAnim: $HoverAnimationPlayer.play_backwards("hover")
+	reset_button_state()
 
 func on_pressed():
 	$AudioStreamPlayer.play()
 	$ClickAnimationPlayer.play("click")
+
+func on_button_hidden():
+	reset_button_state()
