a084b7fac3df26d27428fcf4f18bb2b9649b16dc
MinSungJe <minje813@naver.com>
2023-11-01 17:49:03 +0900

    OptionMenu에 Volume Setting 기능 추가

--- scripts/OptionsMenu.gd
+++ scripts/OptionsMenu.gd
@@ -4,6 +4,8 @@ signal back_pressed
 
 onready var backButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/BackButton
 onready var windowModeButton = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/WindowModeButton
+onready var musicRangeControl = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/MusicVolumeContainer/RangeControl
+onready var sfxRangeControl = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/SFXVolumeContainer/RangeControl
 onready var panelContainerPivot = $MarginContainer/PanelContainer
 var X_
 var Y_
@@ -14,7 +16,10 @@ func _ready():
 	windowModeButton.connect("pressed", self, "on_window_mode_pressed")
 	backButton.connect("pressed", self, "on_back_pressed")
 	fullscreen = OS.is_window_fullscreen()
+	musicRangeControl.connect("percentage_changed", self, "on_music_volume_changed")
+	sfxRangeControl.connect("percentage_changed", self, "on_sfx_volume_changed")
 	update_display()
+	update_initial_volume_settings()
 
 func _process(delta):
 	X_ = panelContainerPivot.rect_size.x / 2
@@ -24,6 +29,19 @@ func _process(delta):
 func update_display():
 	windowModeButton.text = "WINDOWED" if not fullscreen else "FULLSCREEN"	
 
+func update_bus_volume(busName, volumePercent):
+	var busIdx = AudioServer.get_bus_index(busName)
+	var volumeDb = linear2db(volumePercent)
+	AudioServer.set_bus_volume_db(busIdx, volumeDb)
+	
+func get_bus_volume_percent(busName):
+	var busIdx = AudioServer.get_bus_index(busName)
+	var volumePercent = db2linear(AudioServer.get_bus_volume_db(busIdx))
+	return volumePercent
+
+func update_initial_volume_settings():
+	musicRangeControl.set_current_percentage(get_bus_volume_percent("Music"))
+	sfxRangeControl.set_current_percentage(get_bus_volume_percent("SFX"))
 
 func on_window_mode_pressed():
 	fullscreen =  not fullscreen
@@ -32,3 +50,9 @@ func on_window_mode_pressed():
 	
 func on_back_pressed():
 	emit_signal("back_pressed")
+	
+func on_music_volume_changed(percent):
+	update_bus_volume("Music", percent)
+
+func on_sfx_volume_changed(percent):
+	update_bus_volume("SFX", percent)
